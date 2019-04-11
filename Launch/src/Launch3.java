0b9611321a6d56cabff301a18f81ba558655d845
Acyco <769225491@qq.com>
2019-04-11 15:19:27 +0800

    JavaFx Launcher

---
+++ Launch/src/Launch3.java
@@ -0,0 +1,13 @@
+import javafx.application.Application;
+
+/**
+ * 第三种启动方式
+ *
+ * @author Acyco
+ * @create 2019-04-11 15:14
+ */
+public class Launch3 {
+    public static void main(String[] args){
+        Application.launch(LaunchMain.class,args);
+    }
+}
